18f8c32da1f5b0274e9fea8b719e29b97dffb267
İsmail Yarar <107556721+factaxd@users.noreply.github.com>
2024-09-19 17:04:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 class Config:
-    CLIENT_ID = "5r2o4ndvv8pnv7d0rhapda9x9m9gi4"
-    CLIENT_SECRET = "fmlnmeph40pfz75fai03urab0lglgc"
+    CLIENT_ID = "your_twitch_client_id"
+    CLIENT_SECRET = "your_twitch_client_secret"
     REDIRECT_URI = "http://localhost:3000"
-    IRC_TOKEN = "oauth:your_oauth_token"  
-    USERNAME = "your_twitch_username"     
-    CHANNEL = ""  
-    PORT = 3000                           # Yerel sunucu portu
-
-    SCOPE = ['user:read:email', 'chat:read']  # Gerekli izinler
+    IRC_TOKEN = "oauth:your_oauth_token"  # Twitch Chat OAuth Token
+    USERNAME = "your_twitch_username"
+    CHANNEL = "your_twitch_channel"
+    PORT = 3000
+    SCOPE = ['user:read:email', 'chat:read']
